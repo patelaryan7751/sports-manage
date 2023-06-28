1ccc10cbbb8ff8a32774bc5bedb33c6687893476
Aryan Patel <patelaryan7751@gmail.com>
2023-06-28 15:32:43 +0530

    setup basic tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,5 +1,38 @@
-describe("First test suite", () => {
-  test("First test", () => {
-    expect(true).toBe(true);
+const request = require("supertest");
+const cheerio = require("cheerio");
+const db = require("../models/index");
+const app = require("../app");
+
+let server, agent;
+function extractCSRFToken(res) {
+  let $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
+
+describe("Todo Application", function () {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(4000, () => {});
+    agent = request.agent(server);
+  });
+  afterAll(async () => {
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  test("Sign up", async () => {
+    let res = await agent.get("/signup");
+    const csrfToken = extractCSRFToken(res);
+    res = await agent.post("/users").send({
+      name: "Admin",
+      email: "admin@gmail.com",
+      password: "123456",
+      _csrf: csrfToken,
+    });
+    expect(res.statusCode).toBe(302);
   });
 });
